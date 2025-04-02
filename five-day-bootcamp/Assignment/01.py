61de249ad2e0281add83c2b10c1045fed5ec6155
Dhruv <dhruvtyagi88@gmail.com>
2025-04-02 14:11:38 +0530

    first commit

---
+++ five-day-bootcamp/Assignment/01.py
@@ -0,0 +1,37 @@
+##01 --> str is the right answer
+## 02 --> print(2 + 3 * 4)---> answer is 14
+## 03 --> dictionary is the right answer
+## 04 --> if is used for conditional statements
+## 05 --> char is the right answer
+
+
+## A2 06 --> tuple is the ordered collection of items
+## A2 07 --> input() function used to get input from the user console
+## A2 08 --> dictionary is the used to store the data in key-value pairs
+## A2 09 --> pandas us used for data manupulation
+## A2 10 --> int() function is used to convert the data type into integer
+
+
+## A3 11 --> False
+## A3 12 --> True
+## A3 13 --> False
+## A3 14 --> True
+## A3 15 --> False
+
+
+## A3 16 --> len() ---> Returns the number ofitems in an object.
+## A3 17 --> pandas ---> A library for data analysis and manipulation.
+## A3 18 --> type() ---> Returns the data type of an object
+## A3 19 --> append() ---> Used to add an item to a list.
+## A3 10 --> numpy --->  A library for numerical operations.
+
+
+## Even or Odd Checker
+number1 = int(input("Enter a number: "))
+if (number1 % 2 == 0):
+    print(f"{number1} is even")
+else:   
+    print(f"{number1} is odd")
+
+
+##
---
+++ five-day-bootcamp/Day1/Basic.py
@@ -0,0 +1,12 @@
+# add and subtract two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+# add two numbers
+sum = a + b
+print("Sum of a and b is: ", sum)
+
+# subtract two numbers
+diff = a - b   
+print("Difference of a and b is: ", diff)
